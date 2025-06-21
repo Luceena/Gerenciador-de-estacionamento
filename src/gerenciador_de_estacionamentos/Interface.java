c414a941cfbca7859643171f27d5f4a9088217c4
Luceena <julio.rezende15@gmail.com>
2025-06-20 22:52:39 -0300

    Primeiro commit
    Commit inicial

---
+++ src/gerenciador_de_estacionamentos/Interface.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gerenciador_de_estacionamentos;
+
+/**
+ *
+ * @author Júlio Lucena
+ */
+public class Interface {
+    
+}
