54d6ea0910bd68d9bbca093903463c290359632e
Luceena <julio.rezende15@gmail.com>
2025-06-20 23:46:46 -0300

    Alicerces do programa
    Adição dos atributos, construtores, getters e setters das principais classes

---
+++ src/gerenciador_de_estacionamentos/Pessoa.java
@@ -0,0 +1,67 @@
+
+package gerenciador_de_estacionamentos;
+
+import java.util.Objects;
+
+
+public class Pessoa {
+    private String nome;
+    private String cpf;
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "nome=" + nome + ", cpf=" + cpf + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.nome);
+        hash = 79 * hash + Objects.hashCode(this.cpf);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pessoa other = (Pessoa) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        return Objects.equals(this.cpf, other.cpf);
+    }
+    
+    
+    
+    
+    
+}
--- src/gerenciador_de_estacionamentos/Vaga.java
+++ src/gerenciador_de_estacionamentos/Vaga.java
@@ -1,13 +1,109 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package gerenciador_de_estacionamentos;
 
-/**
- *
- * @author Júlio Lucena
- */
+import java.util.Objects;
+
+
 public class Vaga {
+    private int numero;
+    private String tipo;
+    private boolean pcd;
+    private boolean idoso;
+    private boolean ocupada;
+
+    public Vaga(int numero, String tipo, boolean pcd, boolean idoso, boolean ocupada) {
+        this.numero = numero;
+        this.tipo = tipo;
+        this.pcd = pcd;
+        this.idoso = idoso;
+        this.ocupada = ocupada;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public boolean isPcd() {
+        return pcd;
+    }
+
+    public void setPcd(boolean pcd) {
+        this.pcd = pcd;
+    }
+
+    public boolean isIdoso() {
+        return idoso;
+    }
+
+    public void setIdoso(boolean idoso) {
+        this.idoso = idoso;
+    }
+
+    public boolean isOcupada() {
+        return ocupada;
+    }
+
+    public void setOcupada(boolean ocupada) {
+        this.ocupada = ocupada;
+    }
+
+    @Override
+    public String toString() {
+        return "Vaga{" + "numero=" + numero + ", tipo=" + tipo + ", pcd=" + pcd + ", idoso=" + idoso + ", ocupada=" + ocupada + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 79 * hash + this.numero;
+        hash = 79 * hash + Objects.hashCode(this.tipo);
+        hash = 79 * hash + (this.pcd ? 1 : 0);
+        hash = 79 * hash + (this.idoso ? 1 : 0);
+        hash = 79 * hash + (this.ocupada ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Vaga other = (Vaga) obj;
+        if (this.numero != other.numero) {
+            return false;
+        }
+        if (this.pcd != other.pcd) {
+            return false;
+        }
+        if (this.idoso != other.idoso) {
+            return false;
+        }
+        if (this.ocupada != other.ocupada) {
+            return false;
+        }
+        return Objects.equals(this.tipo, other.tipo);
+    }
     
-}
+    
+    
+    
+    
+    
+    }
